a1bb2ff0b29c239b40dfd0fab7d33d174a20d700
DESKTOP-DHEB0UL\User <rksek16@naver.com>
2023-03-14 10:21:29 +0900

    콘솔 계산기 만들기 실습

--- CppPractice/main.cpp
+++ CppPractice/main.cpp
@@ -1,6 +1,34 @@
-#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "Calculator.h"
 
+using namespace std;
 int main()
 {
+	double x = 0.0;
+	double y = 0.0;
+	double result = 0.0;
+	char oper = '+';
 
+	cout << "Calculator Console Apllication" << endl << endl;
+	cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << endl;
+
+	Calculator c;
+	while (true)
+	{
+		cin >> x >> oper >> y;
+		if (oper == '/' && y == 0)
+		{
+			cout << "Division by 0 exception" << endl;
+			continue;
+		}
+		else
+		{
+			result = c.Calculate(x, oper, y);
+		}
+		
+		cout << "Result is: " << result << endl;
+	}
+
+	return 0;
 }
